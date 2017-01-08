7e0028f7286deb71f481644dda34b1ebde7ca6f7
Troy Davis <trda8240@colorado.edu>
2017-01-08 15:10:57 -0700

    updated comment

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from cart import *
 import unittest
 
-#shopping cart class used for py unittest
+#unit tests for cart class
 class ShoppingCartTest(unittest.TestCase):
 	def setUp(self):
 		self.cart = ShoppingCart().add("tuna sandwich", 15.00)
